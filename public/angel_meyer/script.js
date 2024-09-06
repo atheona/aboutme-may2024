eed48c4abf38c5e501d3e676167b1422b9b3fa97
atheona <angel.meyer214@gmail.com>
2024-09-05 20:28:02 -0400

    Some updates.

--- public/angel_meyer/script.js
+++ public/angel_meyer/script.js
@@ -11,7 +11,7 @@ function gradeQuiz(event) {
 
    let petsAnswer = document.querySelector('input[name="animal"]:checked').value;
    if (petsAnswer === "true") {
-      sum += 5
+      sum += 1
       document.getElementById('pet-question').classList.add("correct-answer")
    } else {
       document.getElementById('pet-question').classList.add("wrong-answer")
@@ -19,7 +19,7 @@ function gradeQuiz(event) {
 
    let cityAnswer = document.querySelector('input[name="city"]:checked').value;
    if (cityAnswer === "false") {
-      sum += 5
+      sum += 1
       document.getElementById('city-question').classList.add("correct-answer")
    } else {
       document.getElementById('city-question').classList.add("wrong-answer")
@@ -27,7 +27,7 @@ function gradeQuiz(event) {
 
    let footballAnswer = document.querySelector('input[name="football"]:checked').value;
    if (footballAnswer === "true") {
-      sum += 5
+      sum += 1
       document.getElementById('football-question').classList.add("correct-answer")
    } else {
       document.getElementById('football-question').classList.add("wrong-answer")
@@ -35,13 +35,61 @@ function gradeQuiz(event) {
 
    let photographyAnswer = document.querySelector('input[name="photography"]:checked').value;
    if (photographyAnswer === "false") {
-      sum += 5
+      sum += 1
       document.getElementById('photography-question').classList.add("correct-answer")
    } else {
       document.getElementById('photography-question').classList.add("wrong-answer")
    }
 
-   alert("Total Score: " + sum)
+   let instrumentAnswer = document.querySelector('input[name="instrument"]:checked').value;
+   if (instrumentAnswer === "false") {
+      sum += 1
+      document.getElementById('instrument-question').classList.add("correct-answer")
+   } else {
+      document.getElementById('instrument-question').classList.add("wrong-answer")
+   }
+
+   let holidayAnswer = document.querySelector('input[name="holiday"]:checked').value;
+   if (holidayAnswer === "true") {
+      sum += 1
+      document.getElementById('holiday-question').classList.add("correct-answer")
+   } else {
+      document.getElementById('holiday-question').classList.add("wrong-answer")
+   }
+
+   let travelAnswer = document.querySelector('input[name="travel"]:checked').value;
+   if (travelAnswer === "true") {
+      sum += 1
+      document.getElementById('travel-question').classList.add("correct-answer")
+   } else {
+      document.getElementById('travel-question').classList.add("wrong-answer")
+   }
+   
+   let siblingAnswer = document.querySelector('input[name="sibling"]:checked').value;
+   if (siblingAnswer === "false") {
+      sum += 1
+      document.getElementById('sibling-question').classList.add("correct-answer")
+   } else {
+      document.getElementById('sibling-question').classList.add("wrong-answer")
+   }
+   
+   let boneAnswer = document.querySelector('input[name="bone"]:checked').value;
+   if (boneAnswer === "false") {
+      sum += 1
+      document.getElementById('bone-question').classList.add("correct-answer")
+   } else {
+      document.getElementById('bone-question').classList.add("wrong-answer")
+   }
+
+   let kidsAnswer = document.querySelector('input[name="kids"]:checked').value;
+   if (kidsAnswer === "true") {
+      sum += 1
+      document.getElementById('kids-question').classList.add("correct-answer")
+   } else {
+      document.getElementById('kids-question').classList.add("wrong-answer")
+   }
+
+   // alert("Total Score: " + sum)
 
    // let quizScoreEl = document.getElementById("quiz-score")
    // quizScoreEl.innerHTML = "Total Score " + sum
